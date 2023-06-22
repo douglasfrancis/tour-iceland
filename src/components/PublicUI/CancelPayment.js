8c78656942796e0c49ab6e71a0cb46f6f0faab3d
manylenny <132001471+manylenny@users.noreply.github.com>
2023-06-22 14:38:59 +0100

    Tidy up CancelPayment.js

--- src/components/PublicUI/CancelPayment.js
+++ src/components/PublicUI/CancelPayment.js
@@ -5,18 +5,13 @@ function Cancel() {
     <>
       <h4>Oops! Your payment has been cancelled.</h4>
       <p>
-        We appreciate your business! If you have any questions, please email us
-        at
-        <a href="mailto:orders@example.com">orders@example.com</a>.
-		<br>
-		</br>
-		<a href={'http://localhost:3000/'} target="_blank" rel="noopener noreferrer">
-		Back to home page
-		</a>
+        We appreciate your business! If you have any questions, please email us at 
+        <a href="mailto:orders@example.com"> orders@example.com</a>.
+        <br /><br />
+        <a href={'http://localhost:3000/'} target="_blank" rel="noopener noreferrer">
+          Back to home page
+        </a>
       </p>
-      <div>
-        <button> Go to Home page</button>
-      </div>
     </>
   );
 }
