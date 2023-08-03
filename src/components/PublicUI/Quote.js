7776d6e012e7fdf75a65f9a3b6162723680e68cc
leonard <lennyf@rocketmail.com>
2023-08-03 21:13:55 +0100

    Code adaptions from FDM part 1
    Needs corresponding backend part

--- src/components/PublicUI/Quote.js
+++ src/components/PublicUI/Quote.js
@@ -8,20 +8,21 @@ import moment from 'moment'
 import { loadStripe } from "@stripe/stripe-js";
 
 
+// Display summarised quote information, along with original request, to a client
+// Two scenarios when rendering, with content tailored to each:
+//   - landing here is from an inbox quote (url /quote - or from a page refresh of same)
+//     in this case payment can be made
+//   - landing here is from a successful payment (url /success - or from a page refresh of same)
+//     in this case payment shows as made and when it transitions the booking is stored and emails sent
 export default function Quote() {
-    // two scenarios with content tailored to each
-    //   - landing here is from an inbox quote (url /quote - or from a page refresh of same)
-    //     in this case payment can be made
-    //   - landing here is from a successful payment (url /success - or from a page refresh of same)
-    //     in this case payment shows as made and when it transitions the booking is stored and emails sent
 
     let { id } = useParams()
 
     const [guide, setGuide] = useState("")
     const [request, setRequest] = useState("")
     const [quote, setQuote] = useState("")
-    const [rates, setRates] = useState("")    // Latest currency rates obtained from API
-    const [customerTotal, setCustomerTotal] = useState("")    // customer total in requested currency
+    const [rates, setRates] = useState("")                  // Latest currency rates obtained from API
+    const [customerTotal, setCustomerTotal] = useState("")  // customer total in requested currency
     const [justPaid, setJustPaid] = useState(false)
 
     const location = useLocation();
@@ -29,6 +30,9 @@ export default function Quote() {
 
     useEffect(() => {
         getQuoteInfo()
+    }, [justPaid])
+
+    useEffect(() => {
         getCurrencyRates()
     }, [])
 
@@ -51,24 +55,90 @@ export default function Quote() {
     useEffect(() => {
         // if payment has just been made then create the booking from the request
         // only one booking should ever be created from this
-        if (justPaid && guide && request) {
-            setJustPaid(false)
-
+        if (pathname.includes('/success') && !quote.hasPaid && !justPaid && guide && request) {
+            updateQuoteInfo()
+            setJustPaid(true)
             createBooking()
-            sendBookingConfirmations()
+            updateAndNotifyBookedRequest()
+            notifyQuotingGuides()
         }
-    }, [guide, request, justPaid])
+    }, [guide, request])
+
+    const convertCurrency = () => {
+        // Check if both guideCurrency and clientCurrency exist in rates
+        // guide currency for tour Iceland is always ISK
+        const guideCurrency = "ISK"
+        if (guideCurrency in rates && request.currency in rates) {
+            // NOTE: limitations due to free subscription, currencies always retrieved in USD
+            // Convert ISK to USD first
+            const usdAmount = quote.customerGross / rates[guideCurrency];
+
+            // Convert USD to clientCurrency
+            const convertedAmount = usdAmount * rates[request.currency];
+            setCustomerTotal(Math.round(convertedAmount * 100)/ 100);
+        } else {
+            // Handle case when either guideCurrency or clientCurrency is not found in rates
+            return null;
+        }
+    };
 
     useEffect(() => {
         // convert the currency once we have the rate and request
+        // note that we already have the quote, if we have the request
         if (rates && request) {
             {convertCurrency(quote.customerGross, request.currency, "ISK", rates)}
         }
     }, [rates, request])
 
+    const updateQuoteInfo = async () => {
+        try {
+            const updatedQuote = { ...quote, hasPaid: true };
+            const updatedData = await axios.put(`${process.env.REACT_APP_API}/update-quote/${id}`, { updatedQuote });
+            setQuote(updatedData.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-    const sendBookingConfirmations = async () => {
-        axios.post(`${process.env.REACT_APP_API}/send-booking-confirmations`, { request, guide } )
+    const lookingForMappings = {
+        'Private Tour': 'Private',
+        'Public Tour': 'Group'
+    };
+
+    const createBooking = () => {
+        let payload = {
+            title: `${request.name} - ${request.info.tourType}`,
+            guideId: guide._id,
+            requestId: request._id,
+            date: moment(request.date).format("YYYY-MM-DD"),
+            groupMembers: [request.name],
+            type: lookingForMappings[request.info.lookingFor] || '',
+            notes: request.notes || ""}
+        axios.post(`${process.env.REACT_APP_API}/create-new-booking`, payload)
+        .then(() => {
+            console.log("Booking created for guide")
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    const updateAndNotifyBookedRequest = async () => {
+        // Update the request with the quote Id and guide Id to bind them
+        // If this was from a direct guide request the guide Id may well
+        // already have been present
+        const updatedRequest = { ...request, quoteId: quote._id, guideId: guide._id };
+        let res
+        try {
+            res = await axios.put(`${process.env.REACT_APP_API}/update-request`, { updatedRequest });
+            console.log("Request updated with Ids for guide and quote");
+            setRequest(res.data);
+          } catch (error) {
+            console.log(error);
+        }
+
+        // send notifications
+        axios.post(`${process.env.REACT_APP_API}/send-booking-confirmations`, { request: res.data, guide } )
         .then(() => {
             console.log("Booking confirmation emails sent to client and guide")
         })
@@ -77,25 +147,14 @@ export default function Quote() {
         });
     }
 
-
-    const lookingForMappings = {
-        'Private Tour': 'Private',
-        'Public Tour': 'Group'
-    };
-
-
-    const createBooking = () => {
-        let payload = {
-            title: request.info.tourType,
-            guideId: guide._id,
-            requestId: request._id,
-            date: moment(request.date).format("YYYY-MM-DD"),
-            groupMembers: [request.name], // just the booker name added TBD how/why to know other members
-            type: lookingForMappings[request.info.lookingFor] || '',
-            notes: request.notes || ""}
-        axios.post(`${process.env.REACT_APP_API}/create-new-booking`, payload)
+    // Any guide who has already engaged this client by providing a quote
+    // should be notified that the request is no longer available. Since a
+    // quote is initiated along with an inbox, this can be done by auto
+    // appending a message to the chat
+    const notifyQuotingGuides = async () => {
+        axios.post(`${process.env.REACT_APP_API}/notify-quoting-guides`, { requestId: request._id, guideId: guide._id } )
         .then(() => {
-            console.log("Booking created for guide")
+            console.log("Guides who have provided quotes have now been notified")
         })
         .catch(error => {
             console.log(error);
@@ -120,49 +179,20 @@ export default function Quote() {
 
     const getQuoteInfo = async () => {
         try {
-            const res = await axios.get(`${process.env.REACT_APP_API}/get-quote-by-id/${id}`);
-
-            if (pathname.includes('/success') && !res.data.hasPaid) {
-                const updatedQuote = { ...res.data, hasPaid: true };
-                const updatedData = await axios.put(`${process.env.REACT_APP_API}/update-quote/${id}`, { updatedQuote });
-                setQuote(updatedData.data);
-                setJustPaid(true)
-            }
-            else {
-                setQuote(res.data);
-            }
+            const res = await axios.post(`${process.env.REACT_APP_API}/get-quote-by-id`, {id});
+            setQuote(res.data);
         } catch (error) {
             console.log(error);
         }
     };
 
-
     const [product, setProduct] = useState({
         name: "Guided Tour", // add tour type at least
         price: 0,
-        productOwner: "tourgofer",
-        description: "tour and guide",
+        productOwner: "TourIceland by TourGofer",
+        description: "Tours and Guides of Iceland",
         quantity: 1,
     });
-
-    const convertCurrency = () => {
-        // Check if both guideCurrency and clientCurrency exist in rates
-        // guide currency for tour guide Iceland is always ISK. Maybe add this to .env in future
-        const guideCurrency = "ISK"
-        if (guideCurrency in rates && request.currency in rates) {
-            // limitations due to free subscription, currencies always retrieved in USD
-            // Convert ISK to USD first
-            const usdAmount = quote.customerGross / rates[guideCurrency];
-
-            // Convert USD to clientCurrency
-            const convertedAmount = usdAmount * rates[request.currency];
-            setCustomerTotal(Math.round(convertedAmount * 100)/ 100);
-
-        } else {
-            // Handle case when either guideCurrency or clientCurrency is not found in rates
-            return null;
-        }
-    };
 
     const getCurrencyRates = () => {
         axios.get('https://openexchangerates.org/api/latest.json?app_id=c5a8f0e725fb40d7af7a63857e53eb64')
@@ -173,7 +203,6 @@ export default function Quote() {
             console.log(e)
         });
     }
-
 
     const makePayment = async () => {
         const updatedProduct = { ...product, price: customerTotal, currency: request.currency, quoteId: quote._id };
